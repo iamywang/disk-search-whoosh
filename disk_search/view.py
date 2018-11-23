4c9a2dc311858479d996e9bcb59f17e6316de090
Y.Wang <shengguanglong01@sina.com>
2018-11-23 22:41:25 +0800

    换为pgp message

--- disk_search/view.py
+++ disk_search/view.py
@@ -1,7 +1,11 @@
+import time
+
+import pgpy
 from django.shortcuts import render
 from whoosh.index import open_dir
 from whoosh.qparser import QueryParser
-import time
+
+
 
 
 def index(request):
@@ -9,9 +13,9 @@ def index(request):
     result_list = []
     resnum = 0
     if request.method == "POST":
-        str = request.POST.get("str", None)
-        print("被查询：" + str + "：--disk_search")
-        result_list = search(str)
+        key_word = request.POST.get("str", None)
+        result_list = search(key_word)
+        # skey, _ = pgpy.PGPKey.from_file("file_index/pgp")  # 私钥
         resnum = len(result_list)
     end = time.time()
     return render(request, "../templates/index.html",
@@ -22,15 +26,19 @@ def search(key_word):
     result_list = []
     my_index = open_dir("file_index", indexname='list')
     with my_index.searcher() as searcher:
-        parser = QueryParser("filename", my_index.schema)
-        # parser = QueryParser("filename", my_index.schema)
+        parser1 = QueryParser("filename", my_index.schema)
+        parser2 = QueryParser("file", my_index.schema)
         # parser = QueryParser("filepath", my_index.schema)
-        my_query = parser.parse(key_word)
+        my_query = parser1.parse(key_word) and parser2.parse(key_word)
         results = searcher.search(my_query, limit=None)
         count = 0
+        # key, _ = pgpy.PGPKey.from_file("file_index/pgp.pub")  # 公钥
         for result_item in results:
             count = count + 1
             result_item = dict(result_item)
             result_item["num"] = count
+            result_item["file"] = pgpy.PGPMessage.new(result_item["file"])
+            result_item["filename"] = pgpy.PGPMessage.new(result_item["filename"])
+            result_item["filepath"] = pgpy.PGPMessage.new(result_item["filepath"])
             result_list.append(result_item)
     return result_list
